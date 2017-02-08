5c419d8b2a88a8e894dcce93201aedca2a2024e5
Simon Ransom <simon.ransom@cybercohesion.com>
2017-02-08 11:08:58 +0000

    Added initial .NET core version and DateOfBirth and ISOCountryCode generator

---
+++ src/Faker/DateOfBirth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Faker
+{
+    public class DateOfBirth
+    {
+        private static readonly Random random = new Random(1234);
+
+        private static readonly DateTime MinDate;
+        private static readonly DateTime MaxDate;
+
+        static DateOfBirth()
+        {
+            MinDate = new DateTime(1940, 1, 1);
+            MaxDate = new DateTime(2005, 1, 1);
+        }
+
+        public static DateTime Next()
+        {
+            return new DateTime(random.Next((int)(MaxDate.Ticks - MinDate.Ticks)) + MinDate.Ticks).Date;
+        }
+    }
+}
